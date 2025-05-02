ec271773f11a7a9eeae85d668119135b896972a4
fatima1414 <shaikhfatima5260@gmai.com>
2025-05-02 13:58:56 +0530

    code

---
+++ Basic_calculation/script.js
@@ -0,0 +1,49 @@
+// 1 GRADE SYSTEM
+
+// let grade = 85;
+
+// if (grade >= 90 && grade <= 100) {
+//     console.log(grade, "Your Grade is A. Excellent")
+// }
+// else if (grade >= 80 && grade < 90) {
+//     console.log(grade, "Your Grade is B. Very Good")
+// }
+// else if (grade >= 70 && grade < 80) {
+//     console.log(grade, "Your Grade is C. GOOD")
+// } else if (grade >= 60 && grade < 70) {
+//     console.log(grade, "Your Grade is D. need improvement")
+// } else if (grade >= 50 && grade < 60) {
+//     console.log(grade, "Your Grade is E. Do Hard Work ")
+// }
+// else{
+//     console.log(grade, " You are Fail! Better Luck Next Time ")
+// }
+
+
+// 2  BILL
+
+// let price = 500;
+// let quantity = 4;
+// let tax = 20;
+
+// let total_bill = (price * quantity) + tax;
+
+// console.log(total_bill);
+
+// 3 Employe salary
+
+// let monthly_salary =37000;
+// let year_bonus = 3000;
+// let total_tax = 2000;
+
+// let total = monthly_salary + year_bonus  - total_tax;
+// console.log(total);
+
+// 4 INTREST
+
+// let principal = 1000; 
+// let rate = 5;         
+// let time = 2;  
+
+// const simple_Interest = (principal * rate * time) / 100;
+// console.log( simple_Interest);
